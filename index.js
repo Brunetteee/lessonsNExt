4a54f85a8f980bda392757f3c44870d8353ffa6b
brunetteee <evagelina304@gmail.com>
2024-09-15 22:07:19 +0200

    lesson3

--- index.js
+++ index.js
@@ -1,43 +1,85 @@
-const path = require('node:path')
-const fs = require('node:fs/promises')
+const express = require('express')
+
+const app = express();
+
+const users = [
+    {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
+    {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
+    {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
+    {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
+    {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
+    {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
+    {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
+    {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
+    {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
+    {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
+]
 
 
-const foo = async () => {
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/users', (req, res) => {
     try {
-        const pathToFile = path.join(__dirname, 'baseFolder')
-        await fs.mkdir(pathToFile, {recursive: true})
-
-        const folderNames = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5']
-        const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt',]
-
-        await Promise.all(folderNames.map(async (folder) => {
-            const folderPath = path.join(pathToFile, folder)
-            await fs.mkdir(folderPath, {recursive: true})
-
-            await Promise.all(fileNames.map(async (file) => {
-                const filePath = path.join(folderPath, file)
-                await fs.writeFile(filePath, 'Hello everybody')
-            }))
-        }))
-
-        const data = await fs.readdir(pathToFile)
-        for (const folder of data) {
-            const folderPath = path.join(pathToFile, folder)
-            const stat = await fs.stat(folderPath)
-            console.log(`Folder:${folder},isDirectory:${stat.isDirectory()}`)
-
-            const files = await fs.readdir(pathToFile)
-            for (const file of files) {
-                const pathToFile = path.join(folderPath, file)
-                const fileStat = await fs.stat(pathToFile)
-                console.log(`File:${file},isFile:${fileStat.isFile()}`)
-            }
-        }
+        res.send(users)
 
     } catch (e) {
-        console.log(e.message)
+        res.status(500).send(e.message)
     }
+})
+app.post('/users', (req, res) => {
+    try {
+        const {name, email, password} = req.body
+        const id = users[users.length - 1].id + 1
+        const newUser = {id, name, email, password}
+        users.push(newUser)
+        res.status(201).send(newUser)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+app.get('/users/:userId', (req, res) => {
+    try {
+        const userId = Number(req.params.userId)
+        const user = users.find(user => user.id === userId)
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
 
-}
+app.put('/users/:userId', (req, res) => {
+    try {
+        const userId = Number(req.params.userId)
+        const userIndex = users.findIndex(user => user.id === userId)
+        if (userIndex === -1) {
+            return res.status(404).send('User not found')
+        }
+        const {name, email, password} = req.body
+        users[userIndex] = {...users[userIndex], name, email, password}
+        res.status(201).send(users[userIndex])
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
 
-void foo();
+app.delete('/users/:userId', (req, res) => {
+    try {
+        const userId = Number(req.params.userId)
+        const userIndex = users.findIndex(user => user.id === userId)
+        if (userIndex === -1) {
+            return res.status(404).send('User not found')
+        }
+        users.splice(userIndex, 1)
+        res.sendStatus(204)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+})
