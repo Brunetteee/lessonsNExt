65f5d79cd9d7da40a880146629878803dbaa7eb4
brunetteee <evagelina304@gmail.com>
2024-09-21 21:30:14 +0200

    lesson5

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,15 @@
+import { RoleEnum } from "../enums/role.enum";
+
 export interface IUser {
-  id: number;
+  _id?: string;
   name: string;
   email: string;
   password: string;
+  age: number;
+  phone?: string;
+  role: RoleEnum;
+  isVerify: boolean;
+  isDeleted: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
+import { commonMiddleware } from "../middlewarces/common.middleware";
 
 const router = Router();
 
 router.get("/", userController.getList);
 router.post("/", userController.create);
 
-router.get("/:userId", userController.getById);
-router.put("/:userId", userController.updateById);
-router.delete("/:userId", userController.deleteById);
+router.get(
+  "/:userId",
+  commonMiddleware.isIdValid("userID"),
+  userController.getById,
+);
+router.put(
+  "/:userId",
+  commonMiddleware.isIdValid("userID"),
+  userController.updateById,
+);
+router.delete(
+  "/:userId",
+  commonMiddleware.isIdValid("userID"),
+  userController.deleteById,
+);
 
 export const userRouter = router;
