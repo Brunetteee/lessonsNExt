2c57af3b37d9862a7ebab8eeefc4a0cbacce86a0
brunetteee <evagelina304@gmail.com>
2024-09-22 15:37:48 +0200

    hw5

---
+++ src/validators/user.validator.ts
@@ -0,0 +1,28 @@
+import joi from "joi";
+
+import { regexConstant } from "../constants/regex.constant";
+
+export class UserValidator {
+  private static name = joi.string().min(3).max(20).trim();
+  private static age = joi.number().min(10).max(45);
+  private static email = joi
+    .string()
+    .lowercase()
+    .regex(regexConstant.EMAIL)
+    .trim();
+  private static password = joi.string().regex(regexConstant.PASSWORD).trim();
+  private static phone = joi.string().trim().regex(regexConstant.PHONE);
+
+  public static create = joi.object({
+    name: this.name.required(),
+    age: this.age.required(),
+    email: this.email.required(),
+    password: this.password.required(),
+    phone: this.phone,
+  });
+  public static update = joi.object({
+    name: this.name,
+    age: this.age,
+    phone: this.phone,
+  });
+}
